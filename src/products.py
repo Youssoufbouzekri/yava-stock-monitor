302c0f0694e64048a01b9532252cfd64d81afc27
youssouf <youssouf@gorafeeq.com>
2026-07-29 13:46:21 +0300

    ++

--- src/products.py
+++ src/products.py
@@ -4,7 +4,7 @@ PRODUCTS = [
         "url": "https://www.yavalabs.ae/products/pure-iso-whey-2-kg",
         "variants": [
             {"name": "Banana", "id": 46692232495329},
-            {"name": "White Choco Raspberry", "id": 46692232724705},
+            {"name": "Peanut Butter", "id": 47061618557153},
         ],
     },
 ]
